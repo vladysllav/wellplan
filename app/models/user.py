39270f18417432b5a925ea1880ecc9a9f61d56d9
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-09-19 23:26:43 +0300

    create users model and make migration

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
-
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, Date, Enum
+from sqlalchemy.sql import func
 from app.db.base_class import Base
 
 
@@ -10,4 +10,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
-    # Need to finish this model
+    user_type = Column(Enum('client', 'admin', name='user_type_enum'))
+    date_of_birth = Column(Date(), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
