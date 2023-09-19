b24bdcd9f9d2017912ec560de3e4d319ded43890
Vladyslav Ushakov <ushakov.vladyslav.v@gmail.com>
2023-09-19 14:48:41 +0200

    Add some packages and fix migration from pydantic v1 to pydantic v2 (#2)

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,10 @@ from typing import Any, Dict, List, Optional, Union
 
 from dotenv import load_dotenv
 from pydantic import AnyHttpUrl, HttpUrl, PostgresDsn, field_validator
-from pydantic_core.core_schema import FieldValidationInfo
+
 from pydantic_settings import BaseSettings
+from pydantic_core.core_schema import FieldValidationInfo
+
 
 load_dotenv()
 
