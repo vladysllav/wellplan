ea73294dc1968d699173f0acede1f0303b404ac8
AlexGrig23 <126097780+AlexGrig23@users.noreply.github.com>
2023-09-20 15:36:32 +0300

    create users model and make migration (#3)
    * create users model and make migration
    
    * Fix comments, add usertype usertypeenum

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 import enum
 
-from sqlalchemy import Boolean, Column, Integer, String, Date, Enum
+
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, Date, Enum
+from sqlalchemy.sql import func
 from app.db.base_class import Base
 from app.models.base import TimestampedModel
 
@@ -12,6 +14,7 @@ class UserTypeEnum(enum.Enum):
 
 
 class User(TimestampedModel, Base):
+
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
@@ -20,3 +23,7 @@ class User(TimestampedModel, Base):
     is_active = Column(Boolean(), default=True)
     user_type = Column(Enum(UserTypeEnum), nullable=False, default=UserTypeEnum.client)
     date_of_birth = Column(Date(), nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
