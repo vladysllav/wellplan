2f909a5176279b320851c30eabf1110f473475a2
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-09-20 19:48:08 +0300

    Fix comment for algorithm variable

--- Security.py
+++ Security.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+ALGORITHM = os.getenv("TOKEN_SIGNING_ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
