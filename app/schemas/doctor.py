770b1e85f78f3230b0865edda59dbd0c06aa1ee1
AlexGrig23 <126097780+AlexGrig23@users.noreply.github.com>
2023-09-24 19:25:13 +0300

    Create CRUD endpoints for Doctor and Branc (#6)
    * Create CRUD endpoints for Doctor and Branc
    
    * Fix comments, add status cods, and response_model

---
+++ app/schemas/doctor.py
@@ -0,0 +1,51 @@
+from typing import List
+from datetime import date
+from pydantic import BaseModel
+
+
+class BaseDoctor(BaseModel):
+    first_name: str
+    last_name: str
+    experience: int
+    email: str
+    date_of_birth: date
+    # branch_id: int
+
+
+
+
+class DoctorUpdate(BaseDoctor):
+    middle_name: str
+    description: str
+    profile_image: str
+
+
+class CreateDoctor(BaseDoctor):
+    pass
+
+
+class Doctor(BaseDoctor):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BaseBranch(BaseModel):
+    title: str
+
+
+class CreateBranch(BaseBranch):
+    pass
+
+
+class Branch(BaseBranch):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BranchUpdate(BaseBranch):
+    description: str
+
