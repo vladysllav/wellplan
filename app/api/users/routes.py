59bda0429f99978be252404a64ebce2bbc5be53f
mitatskni <mitatskni@gmail.com>
2023-09-27 12:01:15 +0200

    Some changes

--- app/api/users/routes.py
+++ app/api/users/routes.py
@@ -1,19 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import crud, schemas
+from app import schemas
 from app.api.deps import get_db
 from app.crud.user import crud_user
 router = APIRouter()
+from app.core.security import token_create
 
-
-#
 # @router.get("/profile", response_model=schemas.User)
 # def read_user_profile(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-#     user_info_from_db = crud_user.get_user(db, current_user.id)  # Получите информацию о пользователе из базы данных
+#     user_info_from_db = crud_user.get_user(db, current_user.id)
 #     if user_info_from_db is None:
 #         raise HTTPException(status_code=404, detail="User not found")
 #     return user_info_from_db
-
+#
 
 @router.patch("/{user_id}", response_model=schemas.User)
 def update_user(
@@ -21,8 +20,8 @@ def update_user(
     user_in: schemas.UserUpdate,
     db: Session = Depends(get_db)
 ):
-    user = crud.user.get_user(db, user_id)
+    user = crud_user.user.get_user(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    updated_user = crud.user.update_user(db, user, user_in)
+    updated_user = crud_user.user.update_user(db, user, user_in)
     return updated_user
