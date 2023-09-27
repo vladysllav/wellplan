985a11030da8b7f264ffe1102448342dd549cad7
AlexGrig23 <126097780+AlexGrig23@users.noreply.github.com>
2023-09-27 14:42:10 +0300

    Wel 20 add crud endpoints (#10)
    * Create CRUD endpoints for Doctor and Branc
    
    * Fix comments, add status cods, and response_model
    
    * Fixed cooments for CRUD endpoint
    
    * Fix imports for routers
    
    * Fix comments CRUD endpoints

--- app/api/doctors/routes.py
+++ app/api/doctors/routes.py
@@ -1,66 +1,35 @@
-from typing import Any
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-
-from app.api.auth import routes as auth_routes
-from app.api.deps import get_db
 from app.crud.base import CRUDBase
-from app.api.users import routes as user_routes
+from app.models import Doctor, Branch
+from app.schemas.doctor import CreateDoctor, DoctorUpdate, CreateBranch, BranchUpdate, BaseDoctor, BaseBranch
+from app.api.deps import get_db
 
 
-from app.models import Branch, Doctor
-from app.schemas.doctor import (BaseBranch, BaseDoctor, BranchUpdate,
-                                CreateBranch, CreateDoctor, DoctorUpdate)
-from fastapi import APIRouter
-from app.api.auth import routes
-
-from app.api.auth import routes as auth_routes
-from app.api.doctors import routes as doctor_router
-from app.api.doctors import routes as branch_router
-
-
-api_router = APIRouter()
-
-api_router.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
-api_router.include_router(doctor_router.doctors_router, prefix="/doctors", tags=["doctors"])
-api_router.include_router(branch_router.branches_router, prefix="/branches", tags=["branches"])
-
-
-@api_router.post("/alive")
-def alive() -> Any:
-    """
-    Default alive endpoint
-    """
-    return {"status": "ok"}
-
+doctors_router = APIRouter()
+branches_router = APIRouter()
 
 crud_doctor = CRUDBase(Doctor)
 crud_branch = CRUDBase(Branch)
 
 
-@api_router.post(
-    "/create_docs/", response_model=CreateDoctor, status_code=status.HTTP_201_CREATED
-)
+@doctors_router.post("/doctors/", response_model=CreateDoctor, status_code=status.HTTP_201_CREATED)
 def create_doctor(obj_in: CreateDoctor, db: Session = Depends(get_db)):
     doctor = crud_doctor.create(db, obj_in=obj_in)
 
     return doctor
 
 
-@api_router.get(
-    "/doctors/", response_model=list[BaseDoctor], status_code=status.HTTP_200_OK
-)
+@doctors_router.get("/doctors/", response_model=list[BaseDoctor], status_code=status.HTTP_200_OK)
 def get_doctor_list(db: Session = Depends(get_db)):
     doctors = crud_doctor.get_multi(db)
 
     return doctors
 
 
-@api_router.get(
-    "/doctors/{doctor_id}", response_model=BaseDoctor, status_code=status.HTTP_200_OK
-)
+@doctors_router.get("/doctors/{doctor_id}", response_model=BaseDoctor, status_code=status.HTTP_200_OK)
 def get_doctor(doctor_id: int, db: Session = Depends(get_db)):
     doctor = crud_doctor.get(db, doctor_id)
     if doctor is None:
@@ -69,11 +38,7 @@ def get_doctor(doctor_id: int, db: Session = Depends(get_db)):
     return doctor
 
 
-@api_router.put(
-    "/update_doc/{doctor_id}",
-    response_model=DoctorUpdate,
-    status_code=status.HTTP_200_OK,
-)
+@doctors_router.put("/doctors/{doctor_id}", response_model=DoctorUpdate, status_code=status.HTTP_200_OK)
 def update_doctor(doctor_id: int, obj_in: DoctorUpdate, db: Session = Depends(get_db)):
     doctor = crud_doctor.get(db, doctor_id)
     if doctor is None:
@@ -83,29 +48,24 @@ def update_doctor(doctor_id: int, obj_in: DoctorUpdate, db: Session = Depends(ge
     return doctor
 
 
-@api_router.delete("/delete_doc/{doctor_id}", status_code=status.HTTP_204_NO_CONTENT)
+@doctors_router.delete("/doctors/{doctor_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_doctor(doctor_id: int, db: Session = Depends(get_db)):
     db_obj = crud_doctor.get(db, doctor_id)
     if db_obj is None:
         raise HTTPException(status_code=404, detail="Doctor not found")
-
     db_obj = crud_doctor.remove(db, id=doctor_id)
 
     return db_obj
 
 
-@api_router.get(
-    "/branches/", response_model=list[BaseBranch], status_code=status.HTTP_200_OK
-)
+@branches_router.get("/branches/", response_model=list[BaseBranch], status_code=status.HTTP_200_OK)
 def get_branches_list(db: Session = Depends(get_db)):
     branches = crud_branch.get_multi(db)
 
     return branches
 
 
-@api_router.get(
-    "/branches/{branch_id}", response_model=BaseBranch, status_code=status.HTTP_200_OK
-)
+@branches_router.get("/branches/{branch_id}", response_model=BaseBranch, status_code=status.HTTP_200_OK)
 def get_branches(branch_id: int, db: Session = Depends(get_db)):
     branches = crud_branch.get(db, id=branch_id)
     if branches is None:
@@ -114,18 +74,14 @@ def get_branches(branch_id: int, db: Session = Depends(get_db)):
     return branches
 
 
-@api_router.post(
-    "/create_branch/", response_model=CreateBranch, status_code=status.HTTP_201_CREATED
-)
+@branches_router.post("/branches/", response_model=CreateBranch, status_code=status.HTTP_201_CREATED)
 def create_branch(obj_in: CreateBranch, db: Session = Depends(get_db)):
     branches = crud_branch.create(db, obj_in=obj_in)
 
     return branches
 
 
-@api_router.put(
-    "/branches/{branch_id}", response_model=BranchUpdate, status_code=status.HTTP_200_OK
-)
+@branches_router.put("/branches/{branch_id}", response_model=BranchUpdate, status_code=status.HTTP_200_OK)
 def branch_update(branch_id: int, obj_in: BranchUpdate, db: Session = Depends(get_db)):
     branches = crud_branch.get(db, branch_id)
     if branches is None:
@@ -135,7 +91,7 @@ def branch_update(branch_id: int, obj_in: BranchUpdate, db: Session = Depends(ge
     return branches
 
 
-@api_router.delete("/branches/{branch_id}", status_code=status.HTTP_204_NO_CONTENT)
+@branches_router.delete("/branches/{branch_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_branch(branch_id: int, db: Session = Depends(get_db)):
     branches = crud_branch.get(db, branch_id)
     if branches is None:
