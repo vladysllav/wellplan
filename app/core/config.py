6b65d9ea2c01e72fe12b61aac626abac1c18408b
Vladyslav Ushakov <ushakov.vladyslav.v@gmail.com>
2023-10-04 13:42:41 +0200

    Fix cors allowed origins

--- app/core/config.py
+++ app/core/config.py
@@ -19,11 +19,11 @@ class Settings(BaseSettings):
     AUTHENTICATION__ALGORITHM: str = os.getenv("AUTHENTICATION__ALGORITHM")
 
     SERVER_NAME: str = "wellplan"
-    SERVER_HOST: AnyHttpUrl = "http://localhost:3000"
+    SERVER_HOST: AnyHttpUrl = "http://127.0.0.1:8000"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:3000"]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://127.0.0.1:3000"]
 
     @field_validator("BACKEND_CORS_ORIGINS", mode="before")
     @classmethod
